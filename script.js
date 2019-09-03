ddacd44abca83d8bb1f79473a91cc1aef740e2de
majic1985 <majic1985.mk@gmail.com>
2019-09-03 12:45:27 +0200

    first commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-  var apiRoot = 'https://fierce-badlands-52398.herokuapp.com/v1/task/';
+  var apiRoot = 'https://radiant-sierra-69726.herokuapp.com//v1/task/getTasks';
   var datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   var tasksContainer = $('[data-tasks-container]');
 
